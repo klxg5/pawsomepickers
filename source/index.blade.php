1589c1b83af26566dfc6684d1f82e0333ea8ca3c
Allen <glyakk@gmail.com>
2025-01-18 17:29:20 -0600

    prod build 1

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 <div class="merriweather text-2xl mx-auto mt-12">
-    <img src="{{ rtrim($page->baseUrl, '/') }}/assets/images/PPlogo.svg" />
+    <img src="{{ $page->baseUrl }}" />
     <p class="pt-5 text-center text-black-200 font-black">Reliable pest waste removal serivce in the <br>League City, Pearland, Webster, Friendswood.</p>
 
     <a href="tel:8329147855"><p class="mt-3 py-3 text-center bg-black-200 text-red-300 font-black rounded-full">Call now for a quote: 832.914.7855</p></a>
