c731224e56283e7fd88c1118b9ef13014a1e5909
Allen <glyakk@gmail.com>
2025-01-18 17:14:11 -0600

    update baseUrl in config

--- config.php
+++ config.php
@@ -2,7 +2,16 @@
 
 return [
     'production' => false,
-    'baseUrl' => '',
+    'environments' => [
+        'local' => [
+            'baseUrl' => '/'
+        ],
+        'production' => [
+            'baseUrl' => 'https://klxg5.github.io/pawsomepickers'
+        ],
+    ],
+
+    // 'baseUrl' => '/',
     'title' => 'Pawsome Pickers pet waste removal service',
     'description' => 'Serving League City, Friendwood, Webster, Pearland, and Alvin',
     'collections' => [],
